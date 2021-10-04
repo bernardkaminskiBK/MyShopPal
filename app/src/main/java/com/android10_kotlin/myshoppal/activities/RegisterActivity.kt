824d471fd673ed1bf20477dc734a527ec8c653b7
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2021-10-04 12:46:30 +0200

    Validating registry entries

--- app/src/main/java/com/android10_kotlin/myshoppal/activities/RegisterActivity.kt
+++ app/src/main/java/com/android10_kotlin/myshoppal/activities/RegisterActivity.kt
@@ -3,9 +3,11 @@ package com.android10_kotlin.myshoppal.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import com.android10_kotlin.myshoppal.R
 import com.android10_kotlin.myshoppal.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
 
     private lateinit var mBinding: ActivityRegisterBinding
 
@@ -20,15 +22,61 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+
+        mBinding.btnRegister.setOnClickListener {
+            validateRegisterDetails()
+        }
     }
 
     private fun setupToolbar() {
         setSupportActionBar(mBinding.toolbarRegisterActivity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         mBinding.toolbarRegisterActivity.setNavigationOnClickListener {
             onBackPressed()
         }
     }
 
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(mBinding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            mBinding.etPassword.text.toString().trim { it <= ' ' } != mBinding.etConfirmPassword.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            !mBinding.cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
 }
