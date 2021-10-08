2b1523730fbc047f53e0b29f2b4cf91fc7213c00
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2021-10-08 21:57:58 +0200

    Added settings screen layout

--- app/src/main/java/com/android10_kotlin/myshoppal/ui/activities/SettingsActivity.kt
+++ app/src/main/java/com/android10_kotlin/myshoppal/ui/activities/SettingsActivity.kt
@@ -3,10 +3,27 @@ package com.android10_kotlin.myshoppal.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android10_kotlin.myshoppal.R
+import com.android10_kotlin.myshoppal.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        mBinding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupToolbar()
     }
+
+    private fun setupToolbar() {
+        setSupportActionBar(mBinding.toolbarSettingsActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        mBinding.toolbarSettingsActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
 }
