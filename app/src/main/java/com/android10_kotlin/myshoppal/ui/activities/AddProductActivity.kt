d219dc8cf7b810ffef313e01d3667c2d2c7685a8
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2021-10-13 18:45:09 +0200

    Validating product detail entries

--- app/src/main/java/com/android10_kotlin/myshoppal/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/android10_kotlin/myshoppal/ui/activities/AddProductActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.android10_kotlin.myshoppal.R
@@ -13,7 +14,7 @@ import com.android10_kotlin.myshoppal.utils.Constants
 import com.android10_kotlin.myshoppal.utils.GlideLoader
 import com.android10_kotlin.myshoppal.utils.Utils
 
-class AddProductActivity : AppCompatActivity(), View.OnClickListener {
+class AddProductActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddProductBinding
     private var mSelectedProductPicUri: Uri? = null
@@ -24,6 +25,7 @@ class AddProductActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(mBinding.root)
 
         mBinding.ivAddUpdateProductImage.setOnClickListener(this)
+        mBinding.btnAddProduct.setOnClickListener(this)
 
         setupToolbar()
     }
@@ -44,6 +46,11 @@ class AddProductActivity : AppCompatActivity(), View.OnClickListener {
                     Utils.askForReadPermissionToSavePhoto(this)
                 }
                 R.id.btn_add_product -> {
+                    if(validateProductDetails()) {
+
+                    }
+                }
+                else -> {
 
                 }
             }
@@ -55,11 +62,52 @@ class AddProductActivity : AppCompatActivity(), View.OnClickListener {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == Constants.GALLERY) {
             mSelectedProductPicUri = data!!.data
+
             mBinding.ivAddUpdateProductImage
                 .setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_baseline_edit_34))
+
             GlideLoader(this)
                 .loadPictureIntoView(mSelectedProductPicUri, mBinding.ivProductImage)
         }
     }
+
+    private fun validateProductDetails(): Boolean {
+        return when {
+            mSelectedProductPicUri == null -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_select_product_image), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etProductTitle.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_product_title), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etProductPrice.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_product_price), true)
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etProductDescription.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_product_description),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(mBinding.etProductQuantity.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_product_quantity),
+                    true
+                )
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
 
 }
